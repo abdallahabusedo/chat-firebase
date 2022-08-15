7dce56e8de22cbb1e59815ffe878d7ae93281d62
abdallahabusedo <abozoooz2014.aa@gmail.com>
2022-08-15 17:00:01 +0200

    update config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,6 @@
 import * as firebase from 'firebase/app'
 import { getAuth } from 'firebase/auth';
 import { getFirestore, serverTimestamp } from 'firebase/firestore';
-const firebaseConfig = {
-    apiKey: "AIzaSyB0LKmor7Y7wQepgQCJOnq4mIwz1WWQ2dE",
-    authDomain: "chat-room-firebase-2bdfe.firebaseapp.com",
-    projectId: "chat-room-firebase-2bdfe",
-    storageBucket: "chat-room-firebase-2bdfe.appspot.com",
-    messagingSenderId: "943046024495",
-    appId: "1:943046024495:web:da0acdcd72d47bc6bed5da"
-};
 
 const app = firebase.initializeApp(firebaseConfig);
 
