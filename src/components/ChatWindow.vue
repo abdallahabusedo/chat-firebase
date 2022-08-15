35f392dd3cad8c0a0d91fd257df6aeff71252526
abdallahabusedo <abozoooz2014.aa@gmail.com>
2022-08-15 12:41:24 +0200

    Add auto scroll when add new message

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{error}}</div>
-    <div v-if="Doc" class="messages">
-        <div v-for="doc in Doc" :key="doc.id" class="single">
+    <div v-if="Doc" class="messages" ref="messages">
+        <div v-for="doc in formatedDoc" :key="doc.id" class="single">
             <span class="created-at">{{doc.createdAt}}</span>
             <span class="name">{{doc.name}}</span>
             <span class="message">{{doc.message}}</span>
@@ -12,33 +12,54 @@
 </template>
 
 <script>
-import { onUpdated } from '@vue/runtime-core';
 import getCollection from '../composables/getCollection.js'
-
+import { computed, onUpdated, ref } from 'vue'
+import {formatDistanceToNow} from 'date-fns'
 export default {
-  data(){
-    return{
-      Doc: null,
-      error : null
-    }
-  }, 
-  async created(){
-    await this.detData();
-  },
-  methods:{
-    detData(){
-      const {error , Doc} = getCollection('message')
-      this.Doc = Doc
-      this.error = error
-    }
-  },
-  async onUpdated(){
-    await this.detData()
+   setup() {
+    const { error, Doc } = getCollection('message')
+    const formatedDoc = computed(()=>{
+      if(Doc.value){
+        return Doc.value.map(doc=>{
+          let time = formatDistanceToNow(doc.createdAt.toDate())
+          return {...doc, createdAt: time}
+        })
+      }
+    })
+
+    // auto scroll to last message
+    const messages = ref(null)
+    onUpdated(() => {
+      messages.value.scrollTop = messages.value.scrollHeight
+    })
+
+    return { error, Doc ,formatedDoc , messages}
   }
+  
   
 }
 </script>
 
 <style>
-
+ .chat-window {
+    background: #fafafa;
+    padding: 30px 20px;
+  }
+  .single {
+    margin: 18px 0;
+  }
+  .created-at {
+    display: block;
+    color: #999;
+    font-size: 12px;
+    margin-bottom: 4px;
+  }
+  .name {
+    font-weight: bold;
+    margin-right: 6px;
+  }
+  .messages {
+    max-height: 400px;
+    overflow: auto;
+  }
 </style>
